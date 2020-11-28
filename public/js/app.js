bd4b0cf0b31693070be9c119c84f449ae40c5c15
jpablo1980 <esperix@gmail.com>
2020-11-28 14:17:18 +0100

    Add squares

--- public/js/app.js
+++ public/js/app.js
@@ -3548,50 +3548,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
@@ -47873,7 +47829,7 @@ var staticRenderFns = [
                   {
                     staticClass: "h-full w-full bg-white rounded-lg shadow-2xl"
                   },
-                  [_vm._v("Pikachu")]
+                  [_vm._v("\n          Pikachu\n        ")]
                 )
               ]
             )
@@ -47889,12 +47845,16 @@ var staticRenderFns = [
               "div",
               {
                 staticClass:
-                  "absolute inset-0 transform hover:rotate-90 hover:scale-75 transition duration-300"
+                  "absolute inset-0 transform hover:scale-75 transition duration-300"
               },
               [
-                _c("div", {
-                  staticClass: "h-full w-full bg-white rounded-lg shadow-2xl"
-                })
+                _c(
+                  "div",
+                  {
+                    staticClass: "h-full w-full bg-white rounded-lg shadow-2xl"
+                  },
+                  [_vm._v("\n          Pikachu\n        ")]
+                )
               ]
             )
           ]),
@@ -47909,12 +47869,16 @@ var staticRenderFns = [
               "div",
               {
                 staticClass:
-                  "absolute inset-0 transform hover:rotate-45 transition duration-300"
+                  "absolute inset-0 transform hover:scale-75 transition duration-300"
               },
               [
-                _c("div", {
-                  staticClass: "h-full w-full bg-white rounded-lg shadow-2xl"
-                })
+                _c(
+                  "div",
+                  {
+                    staticClass: "h-full w-full bg-white rounded-lg shadow-2xl"
+                  },
+                  [_vm._v("\n          Pikachu\n        ")]
+                )
               ]
             )
           ]),
@@ -47929,92 +47893,16 @@ var staticRenderFns = [
               "div",
               {
                 staticClass:
-                  "absolute inset-0 transform hover:-rotate-45 transition duration-300"
+                  "absolute inset-0 transform hover:scale-75 transition duration-300"
               },
               [
-                _c("div", {
-                  staticClass: "h-full w-full bg-white rounded-lg shadow-2xl"
-                })
-              ]
-            )
-          ]),
-          _vm._v(" "),
-          _c("div", { staticClass: "h-32 w-32 relative cursor-pointer mb-5" }, [
-            _c("div", {
-              staticClass:
-                "absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl"
-            }),
-            _vm._v(" "),
-            _c(
-              "div",
-              {
-                staticClass:
-                  "absolute inset-0 transform origin-left hover:-rotate-45 transition duration-300"
-              },
-              [
-                _c("div", {
-                  staticClass: "h-full w-full bg-white rounded-lg shadow-2xl"
-                })
-              ]
-            )
-          ]),
-          _vm._v(" "),
-          _c("div", { staticClass: "h-32 w-32 relative cursor-pointer mb-5" }, [
-            _c("div", {
-              staticClass:
-                "absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl"
-            }),
-            _vm._v(" "),
-            _c(
-              "div",
-              {
-                staticClass:
-                  "absolute inset-0 transform hover:-translate-x-10 transition duration-300"
-              },
-              [
-                _c("div", {
-                  staticClass: "h-full w-full bg-white rounded-lg shadow-2xl"
-                })
-              ]
-            )
-          ]),
-          _vm._v(" "),
-          _c("div", { staticClass: "h-32 w-32 relative cursor-pointer mb-5" }, [
-            _c("div", {
-              staticClass:
-                "absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl"
-            }),
-            _vm._v(" "),
-            _c(
-              "div",
-              {
-                staticClass:
-                  "absolute inset-0 transform hover:rotate-90 hover:translate-x-full hover:scale-150 transition duration-300"
-              },
-              [
-                _c("div", {
-                  staticClass: "h-full w-full bg-white rounded-lg shadow-2xl"
-                })
-              ]
-            )
-          ]),
-          _vm._v(" "),
-          _c("div", { staticClass: "h-32 w-32 relative cursor-pointer mb-5" }, [
-            _c("div", {
-              staticClass:
-                "absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl"
-            }),
-            _vm._v(" "),
-            _c(
-              "div",
-              {
-                staticClass:
-                  "absolute inset-0 transform hover:skew-y-12 transition duration-300"
-              },
-              [
-                _c("div", {
-                  staticClass: "h-full w-full bg-white rounded-lg shadow-2xl"
-                })
+                _c(
+                  "div",
+                  {
+                    staticClass: "h-full w-full bg-white rounded-lg shadow-2xl"
+                  },
+                  [_vm._v("\n          Pikachu\n        ")]
+                )
               ]
             )
           ])
--- resources/js/Pages/Pokemon.vue
+++ resources/js/Pages/Pokemon.vue
@@ -45,67 +45,21 @@
         <div
           class="absolute inset-0 transform hover:scale-75 transition duration-300"
         >
-          <div class="h-full w-full bg-white rounded-lg shadow-2xl">Pikachu</div>
+          <div class="h-full w-full bg-white rounded-lg shadow-2xl">
+            Pikachu
+          </div>
         </div>
       </div>
-
-      <!-- roatate and scale -->
       <div class="h-32 w-32 relative cursor-pointer mb-5">
         <div
           class="absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl"
         ></div>
         <div
-          class="absolute inset-0 transform hover:rotate-90 hover:scale-75 transition duration-300"
+          class="absolute inset-0 transform hover:scale-75 transition duration-300"
         >
-          <div class="h-full w-full bg-white rounded-lg shadow-2xl"></div>
-        </div>
-      </div>
-
-      <!-- rotate -->
-      <div class="h-32 w-32 relative cursor-pointer mb-5">
-        <div
-          class="absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl"
-        ></div>
-        <div
-          class="absolute inset-0 transform hover:rotate-45 transition duration-300"
-        >
-          <div class="h-full w-full bg-white rounded-lg shadow-2xl"></div>
-        </div>
-      </div>
-
-      <!-- rotate minus -->
-      <div class="h-32 w-32 relative cursor-pointer mb-5">
-        <div
-          class="absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl"
-        ></div>
-        <div
-          class="absolute inset-0 transform hover:-rotate-45 transition duration-300"
-        >
-          <div class="h-full w-full bg-white rounded-lg shadow-2xl"></div>
-        </div>
-      </div>
-
-      <!-- Origin -->
-      <div class="h-32 w-32 relative cursor-pointer mb-5">
-        <div
-          class="absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl"
-        ></div>
-        <div
-          class="absolute inset-0 transform origin-left hover:-rotate-45 transition duration-300"
-        >
-          <div class="h-full w-full bg-white rounded-lg shadow-2xl"></div>
-        </div>
-      </div>
-
-      <!-- translate -->
-      <div class="h-32 w-32 relative cursor-pointer mb-5">
-        <div
-          class="absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl"
-        ></div>
-        <div
-          class="absolute inset-0 transform hover:-translate-x-10 transition duration-300"
-        >
-          <div class="h-full w-full bg-white rounded-lg shadow-2xl"></div>
+          <div class="h-full w-full bg-white rounded-lg shadow-2xl">
+            Pikachu
+          </div>
         </div>
       </div>
 
@@ -113,23 +67,25 @@
         <div
           class="absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl"
         ></div>
-
         <div
-          class="absolute inset-0 transform hover:rotate-90 hover:translate-x-full hover:scale-150 transition duration-300"
+          class="absolute inset-0 transform hover:scale-75 transition duration-300"
         >
-          <div class="h-full w-full bg-white rounded-lg shadow-2xl"></div>
+          <div class="h-full w-full bg-white rounded-lg shadow-2xl">
+            Pikachu
+          </div>
         </div>
       </div>
 
-      <!-- skew -->
       <div class="h-32 w-32 relative cursor-pointer mb-5">
         <div
           class="absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl"
         ></div>
         <div
-          class="absolute inset-0 transform hover:skew-y-12 transition duration-300"
+          class="absolute inset-0 transform hover:scale-75 transition duration-300"
         >
-          <div class="h-full w-full bg-white rounded-lg shadow-2xl"></div>
+          <div class="h-full w-full bg-white rounded-lg shadow-2xl">
+            Pikachu
+          </div>
         </div>
       </div>
     </section>
